d86ce5ce55f81a8973b7f75e073110baf31ff905
Michael Palassis <m776829@8451.com>
2020-09-22 14:46:12 -0400

    tweaking foreground

--- visualise-docker/foreground.sh
+++ visualise-docker/foreground.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-bgfile=/tmp/background.done
-echo "waiting for background script to complete..."
+#bgfile=/tmp/background.done
+#echo "waiting for background script to complete..."
+#
+#until [ -f $bgfile ]
+#do
+#     sleep 5
+#done
+#echo "File found"
+#exit
 
-until [ -f $bgfile ]
-do
-     sleep 5
-done
-echo "File found"
-exit
 
-
-which termdown
-which httpie
+#which termdown
+#which httpie
 git clone https://github.com/dockersamples/example-voting-app.git
 cd example-voting-app/
-docker-compose up -d
-for i in $(seq 1 100); do VAL=$(shuf -n 1 -e a b); echo "VOTING: $VAL"; http --ignore-stdin -h -f POST "localhost:5000" vote=$VAL; done
+#docker-compose up -d
+#for i in $(seq 1 100); do VAL=$(shuf -n 1 -e a b); echo "VOTING: $VAL"; http --ignore-stdin -h -f POST "localhost:5000" vote=$VAL; done
 #termdown -b 55m -f standard -t "< 5   MINS   LEFT!"
