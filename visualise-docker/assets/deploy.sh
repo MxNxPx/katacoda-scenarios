566c48f1521953169e77d69905f9fa02f24759f8
Michael Palassis <m776829@8451.com>
2020-09-23 06:36:38 -0400

    testing use of assets

---
+++ visualise-docker/assets/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y httpie stress
+pip install termdown
+curl -L https://k14s.io/install.sh | bash
+
+
+git clone https://github.com/dockersamples/example-voting-app.git
+cd example-voting-app/
+clear
+cat<<'EOF' >/root/launch-tmux.sh
+#!/bin/sh
+
+# Set Session Name
+SESSION="tmux-term"
+SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+# Only create tmux session if it doesn't already exist
+if [ "$SESSIONEXISTS" = "" ]
+then
+    # Start New Session with our name
+    tmux new-session -d -s $SESSION
+    tmux selectp -t 0    # select the first (0) pane
+    tmux splitw -v -p 10 # split it into two halves
+    tmux send-keys 'termdown -b 55m -f standard -t "< 5   MINS   LEFT!"' C-m
+
+    tmux selectp -t 0    # select the first (0) pane
+    tmux send-keys 'clear' C-m # Switch to bind script?
+fi
+
+# Attach Session, on the Main window
+tmux attach-session -t $SESSION:0
+EOF
+
+cd /root/
+touch hello-world
--- visualise-docker/foreground.sh
+++ visualise-docker/foreground.sh
@@ -1,38 +1,6 @@
-#!/bin/bash
+sleep 1; /usr/local/bin/wait.sh
 
-apt-get update
-apt-get install -y httpie stress
-pip install termdown
-curl -L https://k14s.io/install.sh | bash
-
-
-git clone https://github.com/dockersamples/example-voting-app.git
-cd example-voting-app/
-clear
-cat<<'EOF' >/root/launch-tmux.sh
-#!/bin/sh
-
-# Set Session Name
-SESSION="tmux-term"
-SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
-
-# Only create tmux session if it doesn't already exist
-if [ "$SESSIONEXISTS" = "" ]
-then
-    # Start New Session with our name
-    tmux new-session -d -s $SESSION
-    tmux selectp -t 0    # select the first (0) pane
-    tmux splitw -v -p 10 # split it into two halves
-    tmux send-keys 'termdown -b 55m -f standard -t "< 5   MINS   LEFT!"' C-m
-
-    tmux selectp -t 0    # select the first (0) pane
-    tmux send-keys 'clear' C-m # Switch to bind script?
-fi
-
-# Attach Session, on the Main window
-tmux attach-session -t $SESSION:0
-EOF
-bash /root/launch-tmux.sh
+#bash /root/launch-tmux.sh
 
 #docker-compose up -d
 #nohup for i in $(seq 1 100); do VAL=$(shuf -n 1 -e a b); echo "VOTING: $VAL"; http --ignore-stdin -h -f POST "localhost:5000" vote=$VAL; done &
