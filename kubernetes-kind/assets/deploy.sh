294fc31f556d27747ca23fa61ea088b4b8d28dca
Michael Palassis <m776829@8451.com>
2020-10-02 17:12:23 -0400

    tweaking terminal experience

--- kubernetes-kind/assets/deploy.sh
+++ kubernetes-kind/assets/deploy.sh
@@ -13,10 +13,22 @@ apt-get update \
 && apt-get update \
 && apt-get install -y kubectl httpie
 
-pip install termdown
 
 ## install kind and start cluster
 go get -u -v sigs.k8s.io/kind \
 && kind create cluster --config /root/kind-2worker-ephemeral-config.yaml --image kindest/node:v1.16.15 --wait 5m && kubectl wait --timeout=5m --for=condition=Ready nodes --all
+
+cat <<'EOF' >/root/.bash_profile
+cat ~/.bash_profile
+prompt_command() {
+  _PS1_expire=3555
+  _PS1_now=$(expr `date +%s` - `stat -c %Y /tmp/d`)
+  PS1=$( printf "\[\e[0;32m\]%02d:%02d \W>\[\e[1;37m\] " \
+           $(( (( _PS1_expire - _PS1_now ) % 3600) / 60 )) \
+           $((  ( _PS1_expire - _PS1_now ) % 60))           \
+       )
+}
+PROMPT_COMMAND='prompt_command'
+EOF
 
 echo "deploy complete"
