60697a5ca592cacaf9d53e2c313aed737c0c7d77
MxNxPx <MxNxPx@gmail.com>
2020-10-03 19:35:10 -0400

    testing if kubectl is installed via k3s

--- kubernetes-k3s/assets/deploy.sh
+++ kubernetes-k3s/assets/deploy.sh
@@ -7,11 +7,12 @@
 
 ## packages (ex: kubectl httpie termdown)
 apt-get update \
-&& apt-get install -y apt-transport-https \
-&& curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - \
-&& echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list \
+#&& apt-get install -y apt-transport-https \
+#&& curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - \
+#&& echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list \
 && apt-get update \
-&& apt-get install -y kubectl httpie
+#&& apt-get install -y kubectl httpie
+&& apt-get install -y httpie
 
 
 ## install k3s and start cluster
