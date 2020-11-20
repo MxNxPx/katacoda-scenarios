557e6818c4675ed8f33d9a01e61aaf1b67975fc2
Michael Palassis <m776829@8451.com>
2020-11-20 12:32:31 -0500

    ytt moved repos, updating install script

--- visualise-docker/assets/deploy.sh
+++ visualise-docker/assets/deploy.sh
@@ -5,7 +5,7 @@ apt-get install -y httpie stress
 pip install termdown
 
 cd /root
-wget $(curl -s https://api.github.com/repos/k14s/ytt/releases/latest | egrep "browser_download_url.*linux.*64" | cut -d '"' -f 4) && mv ytt*linux*64 /usr/local/bin/ytt && chmod 755 /usr/local/bin/ytt
+wget $(curl -s https://api.github.com/repos/vmware-tanzu/carvel-ytt/releases/latest | egrep "browser_download_url.*linux.*64" | cut -d '"' -f 4) && mv ytt*linux*64 /usr/local/bin/ytt && chmod 755 /usr/local/bin/ytt
 
 git clone https://github.com/dockersamples/example-voting-app.git
 cp -pfv ./example-voting-app/docker-compose.yml{,.orig}
